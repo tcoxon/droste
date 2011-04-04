67226b3187b53fbaa179959a4544cd8a469d7cbf
Tom Coxon <tom.c.coxon@googlemail.com>
2011-04-04 23:08:57 +0100

    Transform into polar coordinates. Reverse transformation is commented out.

--- main.c
+++ main.c
@@ -5,6 +5,8 @@
 #include <stdio.h>
 #include <string.h>
 
+#include <math.h>
+
 typedef uint8_t uint8;
 typedef uint16_t uint16;
 typedef uint32_t uint32;
@@ -257,11 +259,35 @@ void transform(Pixel *obmp, Pixel *ibmp) {
     printf("Transparent (hex color): #%02x%02x%02x\n",
         transpColor.r, transpColor.g, transpColor.b);
 
+    const double two_pi = 2.0 * M_PI;
+    double t_yo_scale = two_pi / height;
+    double r_xo_scale = sqrt(width * width + height * height) / width;
     for (i = 0; i < width*height; i++) {
-        if (!pixel_eq(ibmp[i], transpColor)) {
-            obmp[i] = ibmp[i];
+        int xo = i % width,
+            yo = i / width;
+        double t = (yo + 0.0) * t_yo_scale,
+               r = (xo + 0.0) * r_xo_scale;
+               
+        double xi = r*cos(t) + width/2,
+               yi = r*sin(t) + height/2;
+
+        /* Reverse transform */
+        /*{
+            xi -= width/2;
+            yi -= height/2;
+            r = sqrt(xi*xi + yi*yi);
+            t = atan2(yi, xi);
+            if (t < 0) t += two_pi;
+            xi = r / r_xo_scale;
+            yi = t / t_yo_scale;
+        }*/
+
+        if (0 <= xi && xi < width &&
+            0 <= yi && yi < height)
+        {
+            obmp[i] = ibmp[(int)xi + ((int)yi)*width];
         } else {
-            obmp[i] = pixel(0, 0, 0);
+            obmp[i] = pixel(0xff,0,0);//transpColor;
         }
     }
 
