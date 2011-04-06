85ab239d3d943bd5f301accd4a6bf5d683013720
Tom Coxon <tom.c.coxon@googlemail.com>
2011-04-06 21:18:40 +0100

    Reorganise variables a little.

--- droste.c
+++ droste.c
@@ -17,6 +17,24 @@ int pixel_eq(Pixel x, Pixel y) {
     return x.r == y.r && x.g == y.g && x.b == y.b;
 }
 
+/* Variables that are constant across a single image */
+static const double two_pi = 2.0 * M_PI;
+static double origin_x, origin_y;
+static double r1, r2, r2_over_r1;
+static double a, period, cosa, sina, cosacosa, cosasina;
+
+/* Rotate the coordinates so that diagonal coincides with the imaginary
+   axis and shrunk to be 2pi high.
+    Where z = x+jy, and a = atan(log(r2/r1)/2pi),
+    z' = z cos(a) e**ja
+    or  x' = x cos(a) cos(a) - y cos(a) sin(a)
+        y' = y cos(a) cos(a) + x cos(a) sin(a)
+ */
+void rotate(double *rx, double *ry, double x, double y) {
+    *rx = x*cosacosa - y*cosasina;
+    *ry = y*cosacosa + x*cosasina;
+}
+
 /* Transform x and y into logarithmic polar coordinates:
     where z = x+jy,
     z' = log(z)
@@ -26,33 +44,6 @@ int pixel_eq(Pixel x, Pixel y) {
 void to_logpolar(double *rx, double *ry, double x, double y) {
     *rx = log(sqrt(x*x + y*y));
     *ry = atan2(y, x);
-}
-
-/* Scaling factor between one iteration and the next one out.
-   For the moment, just assume it's always 1.7. */
-static const double r2_over_r1 = 1.7;
-static const double two_pi = 2.0 * M_PI;
-
-/* Rotate the coordinates so that diagonal coincides with the imaginary
-   axis and shrunk to be 2pi high.
-    Where z = x+jy, and a = atan(log(r2/r1)/2pi),
-    z' = z cos(a) e**ja
-    or  x' = x cos(a) cos(a) - y cos(a) sin(a)
-        y' = y cos(a) cos(a) + x cos(a) sin(a)
- */
-static double a, period, cosa, sina, cosacosa, cosasina;
-void rotate(double *rx, double *ry, double x, double y) {
-    *rx = x*cosacosa - y*cosasina;
-    *ry = y*cosacosa + x*cosasina;
-}
-
-void init_transform(void) {
-    period = log(r2_over_r1);
-    a = atan2(period, two_pi);
-    cosa = cos(a);
-    sina = sin(a);
-    cosacosa = cosa*cosa;
-    cosasina = cosa*sina;
 }
 
 /* Transform log-polar coords x and y into cartesian coordinates.
@@ -66,14 +57,51 @@ void to_cartes(double *rx, double *ry, double x, double y) {
     *ry = sin(y) * e_x;
 }
 
+void init_transform(Pixel *bmp) {
+    transpColor = bmp[width*height/2 + width/2];
+    printf("Transparent (hex color): #%02x%02x%02x\n",
+        transpColor.r, transpColor.g, transpColor.b);
+
+    /* Centre of the transformation, currently always at
+       the centre of the image, but could really be anywhere */
+    origin_x = width/2.0;
+    origin_y = height/2.0;
+    printf("Centre at (%f, %f)\n", origin_x, origin_y);
+
+    /* Calculate r2_over_r1 (used to calculate the period of repetition).
+       Currently assumes the origin is at the centre of the image.
+       r2 is the outer radius (beyond which may lie transparent pixels).
+       r1 is the inner radius (within which may lie transparent pixels). */
+    r2 = origin_y < origin_x ? origin_y : origin_x;
+
+    int i;
+    r1 = 0.0;
+    for (i = 0; i < width * height; i++) {
+        if (pixel_eq(bmp[i], transpColor)) {
+            double x = i % width - origin_x,
+                   y = i / width - origin_y;
+            double r = sqrt(x*x + y*y);
+            if (r > r1)
+                r1 = r;
+        }
+    }
+    printf("r2 = %f, r1 = %f\n", r2, r1);
+    
+    r2_over_r1 = r2/r1;
+
+    /* Set up some values that are constant across the image */
+    period = log(r2_over_r1);
+    a = atan2(period, two_pi);
+    cosa = cos(a);
+    sina = sin(a);
+    cosacosa = cosa*cosa;
+    cosasina = cosa*sina;
+}
+
 void transform(Pixel *obmp, Pixel *ibmp) {
     int i;
 
-    init_transform();
-
-    transpColor = ibmp[width*height/2 + width/2];
-    printf("Transparent (hex color): #%02x%02x%02x\n",
-        transpColor.r, transpColor.g, transpColor.b);
+    init_transform(ibmp);
 
     for (i = 0; i < width*height; i++) {
         int j, k;
@@ -82,19 +110,25 @@ void transform(Pixel *obmp, Pixel *ibmp) {
 
         obmp[i] = transpColor;
 
-        x -= width/2;
-        y -= height/2;
+        x -= origin_x;
+        y -= origin_y;
 
-        /* Do x and y need to be scaled here? */
+#if 0
+        /* display the image in log-polar coordinates */
+        x *= log(sqrt(width*width+height*height)) * 4 / width;
+        y *= two_pi / height;
+#else
         to_logpolar(&x, &y, x, y);
         rotate(&x, &y, x, y);
+#endif
 
         for (j = -2; j < 10; j++) {
             double x1 = x, y1 = y;
-            to_cartes(&x1, &y1, x1 + period*j, y1);
+            x1 += period*j;
+            to_cartes(&x1, &y1, x1, y1);
 
-            x1 += width/2;
-            y1 += height/2;
+            x1 += origin_x;
+            y1 += origin_y;
 
             if (0 <= x1 && x1 < width &&
                 0 <= y1 && y1 < height)
